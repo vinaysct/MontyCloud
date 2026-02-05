31fc35a410c47941f328bcac4f9bf689a0916ee5
vinaysct <vinny503@gmail.com>
2026-02-05 23:20:03 +0530

    Add files via upload

--- montycloud-image-service/init_localstack.sh
+++ montycloud-image-service/init_localstack.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-awslocal s3 mb s3://image-bucket
+awslocal s3 mb s3://montycloud-image
 
-awslocal dynamodb create-table   --table-name images_metadata   --attribute-definitions     AttributeName=image_id,AttributeType=S     AttributeName=user_id,AttributeType=S   --key-schema AttributeName=image_id,KeyType=HASH   --global-secondary-indexes     '[{
+awslocal dynamodb create-table   --table-name montycloud_images_metadata   --attribute-definitions     AttributeName=image_id,AttributeType=S     AttributeName=user_id,AttributeType=S   --key-schema AttributeName=image_id,KeyType=HASH   --global-secondary-indexes     '[{
       "IndexName":"user_id-index",
       "KeySchema":[{"AttributeName":"user_id","KeyType":"HASH"}],
       "Projection":{"ProjectionType":"ALL"}
